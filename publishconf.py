775f7109da16047ae816e91f855582f974574b88
Nicolas Lœuillet <nicolas@loeuillet.org>
2014-12-09 11:29:28 +0100

    forgot one config file

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.wallabag.org'
+SITEURL = 'https://www.wallabag.org'
 RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = True
